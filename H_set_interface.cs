109d0ababe458b713cd1b65d6ce3ed41131a3a11
Gayatri Gawali <135634500+Gayatrigawali07@users.noreply.github.com>
2026-03-11 10:13:00 +0530

    Uncomment H_set_interface methods and logic

--- H_set_interface.cs
+++ H_set_interface.cs
@@ -1,51 +1,52 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
+//In a fruit collection, remove the object of 4 (while iterating). Which index removes the fruit?
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-//class H_set_interface
-//{
-//    HashSet<int> fruits;
+class H_set_interface
+{
+   HashSet<int> fruits;
 
-//    // Create fruit objects
-//    void FruitObjects()
-//    {
-//        fruits = new HashSet<int>();
-//        fruits.Add(1);
-//        fruits.Add(2);
-//        fruits.Add(3);
-//        fruits.Add(4);
-//        fruits.Add(5);
-//    }
+   // Create fruit objects
+   void FruitObjects()
+   {
+       fruits = new HashSet<int>();
+       fruits.Add(1);
+       fruits.Add(2);
+       fruits.Add(3);
+       fruits.Add(4);
+       fruits.Add(5);
+   }
 
-//    // Remove fruit "4" safely while iterating
-//    void RemoveFruit()
-//    {
-//        // Iterate over a copy to remove safely
-//        foreach (int f in fruits.ToList())
-//        {
-//            if (f == 4)
-//            {
-//                fruits.Remove(f);
-//            }
-//        }
-//    }
+   // Remove fruit "4" safely while iterating
+   void RemoveFruit()
+   {
+       // Iterate over a copy to remove safely
+       foreach (int f in fruits.ToList())
+       {
+           if (f == 4)
+           {
+               fruits.Remove(f);
+           }
+       }
+   }
 
-//    // Display all fruits
-//    void DisplayFruits()
-//    {
-//        foreach (int f in fruits)
-//        {
-//            Console.WriteLine(f);
-//        }
-//    }
+   // Display all fruits
+   void DisplayFruits()
+   {
+       foreach (int f in fruits)
+       {
+           Console.WriteLine(f);
+       }
+   }
 
-//    // Main method
-//    public static void Main(string[] args)
-//    {
-//        H_set_interface ob = new H_set_interface();
-//        ob.FruitObjects();
-//        ob.RemoveFruit();
-//        ob.DisplayFruits();
-//    }
-//}
+   // Main method
+   public static void Main(string[] args)
+   {
+       H_set_interface ob = new H_set_interface();
+       ob.FruitObjects();
+       ob.RemoveFruit();
+       ob.DisplayFruits();
+   }
+}
 
